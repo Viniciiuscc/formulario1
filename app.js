ba668f04fcf30091fd8943450e493671a2e0d9b6
Vinícius Ribeiro Silva <130311846+Viniciiuscc@users.noreply.github.com>
2023-04-09 19:26:46 -0300

    Update app.js

--- app.js
+++ app.js
@@ -24,33 +24,3 @@ function addForm(){
     inputName.value = inputCity.value = inputBio.value = inputState.value = ''
     
 }
-
-
-const inputFile = document.querySelector('#formFoto')
-const pictureImage = document.querySelectorTxt = (".formFoto")
-const pictureImageTxt = "foto de perfil"
-pictureImage.innerHTML = pictureImageTxt
-
-inputFile.addEventListener("change" , function(e) {
-    const inputTarget = e.target
-    const file = inputTarget.files[0]
-
-    if(file){
-        const reader = new FileReader()
-
-        reader.addEventListener("load", function(e) {
-            const readerTarget = e.target
-
-            const img = document.createElement('img')
-            img.src = readerTarget.result
-            img.classList.add("formFoto")
-
-            pictureImage.innerHTML = ''
-            pictureImage.appendChild(img)
-        })
-
-        reader.readAsDataURL(file);
-    }   else {
-        pictureImage.innerHTML = pictureImageTxt
-    }
-})
